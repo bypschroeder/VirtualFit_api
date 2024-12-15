13a743c6d4bc2b8007f91dff69da06a85e59894d
bypschroeder <patrick.schroeder@hof-university.de>
2024-12-15 19:29:36 +0100

    fix volume

--- main.py
+++ main.py
@@ -30,8 +30,7 @@ def generate_3d_model():
         # image_path = os.path.join(images_folder, "sample.jpg")
         # image_file.save(image_path)
 
-        result = client.containers.run("openpose", device_requests=[docker.types.DeviceRequest(count=-1, capabilities=[["gpu"]])], command=f"--image_dir {DATA_FOLDER}/images --write_json {DATA_FOLDER}/keypoints --face --hand --display 0 --render_pose 0", remove=True, volumes={f"DATA_FOLDER": {'bind': DATA_FOLDER, 'mode': 'rw'}})
-        print(result)
+        result = client.containers.run("openpose", device_requests=[docker.types.DeviceRequest(count=-1, capabilities=[["gpu"]])], command=f"--image_dir {DATA_FOLDER}/images --write_json {DATA_FOLDER}/keypoints --face --hand --display 0 --render_pose 0", remove=True, volumes={DATA_FOLDER: {'bind': DATA_FOLDER, 'mode': 'rw'}})
 
         return jsonify({
             "message": "OpenPose execution successful",
