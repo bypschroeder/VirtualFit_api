17646edfb11e31f8bc390109f045d3dccfca4778
bypschroeder <patrick.schroeder@hof-university.de>
2024-12-15 16:08:22 +0100

    updated docker run

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 DATA_FOLDER = '/data'
 
-docker_client = docker.from_env()
+client = docker.from_env()
 
 @app.route("/")
 def index():
@@ -15,32 +15,26 @@ def index():
 @app.route('/generate-3d-model', methods=['POST'])
 def generate_3d_model():
     try:
-        if "image" not in request.files:
-            return jsonify({'error': 'No image file provided'}), 400
+        # if "image" not in request.files:
+        #     return jsonify({'error': 'No image file provided'}), 400
         
-        image_file = request.files['image']
+        # image_file = request.files['image']
 
-        if image_file.filename == '':
-            return jsonify({'error': 'No image file provided'}), 400
+        # if image_file.filename == '':
+        #     return jsonify({'error': 'No image file provided'}), 400
         
-        images_folder = os.path.join(DATA_FOLDER, 'images')
-        os.makedirs(images_folder, exist_ok=True)
+        # images_folder = os.path.join(DATA_FOLDER, 'images')
+        # os.makedirs(images_folder, exist_ok=True)
 
-        image_path = os.path.join(images_folder, "sample.jpg")
-        image_file.save(image_path)
+        # image_path = os.path.join(images_folder, "sample.jpg")
+        # image_file.save(image_path)
 
-        openpose = docker_client.containers.get("openpose")
-        openpose_command = f"/openpose/build/examples/openpose/openpose.bin --image_dir {DATA_FOLDER}/images --write_json {DATA_FOLDER}/keypoints --display 0 --write_images 0"
-        print(openpose_command)
-        openpose_result = openpose.exec_run(openpose_command, stdout=True, stderr=True)
-        print(openpose_result)
+        result = client.containers.run("openpose/openpose:latest", command=f"--image_dir {DATA_FOLDER}/images --write_json {DATA_FOLDER}/keypoints --display 0 --write_images 0", remove=True)
+        print(result)
 
-        if openpose_result.exit_code != 0:
-            return jsonify({'error': 'OpenPose execution failed'}), 400
-        
         return jsonify({
             "message": "OpenPose execution successful",
-            "output": openpose_result.output.decode("utf-8")
+            "output": result.output.decode("utf-8")
         }), 200
     except docker.errors.NotFound:
         return jsonify({'error': 'OpenPose container not found'}), 404
